7efcb96ec9a8f02073770298b0f3b6a665c06ebe
KylerSmith <kylerasmith@gmail.com>
2016-11-10 09:54:14 -0700

    Add files via upload
    Card classes

---
+++ Card.java
@@ -0,0 +1,12 @@
+
+public class Card {
+	// properties
+	private String color;
+	
+	// accessors and mutators
+	public String getColor() { return color; }
+	public void setColor(String pColor) { color = pColor; }
+}
+
+
+
