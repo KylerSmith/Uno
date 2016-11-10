7a12eb9816f4c263b0cd1949c32e8c9a2f26ada1
KylerSmith <kylerasmith@gmail.com>
2016-11-09 17:38:58 -0700

    Add files via upload
    Hard-coded all cards here

---
+++ AllCards.java
@@ -0,0 +1,157 @@
+
+
+
+public class AllCards {
+	
+	// yellow cards
+	NumberCard y00 = new NumberCard("yellow", 0);
+	NumberCard y11 = new NumberCard("yellow", 1);
+	NumberCard y12 = new NumberCard("yellow", 1);
+	NumberCard y21 = new NumberCard("yellow", 2);
+	NumberCard y22 = new NumberCard("yellow", 2);
+	NumberCard y31 = new NumberCard("yellow", 3);
+	NumberCard y32 = new NumberCard("yellow", 3);
+	NumberCard y41 = new NumberCard("yellow", 4);
+	NumberCard y42 = new NumberCard("yellow", 4);
+	NumberCard y51 = new NumberCard("yellow", 5);
+	NumberCard y52 = new NumberCard("yellow", 5);
+	NumberCard y61 = new NumberCard("yellow", 6);
+	NumberCard y62 = new NumberCard("yellow", 6);
+	NumberCard y71 = new NumberCard("yellow", 7);
+	NumberCard y72 = new NumberCard("yellow", 7);
+	NumberCard y81 = new NumberCard("yellow", 8);
+	NumberCard y82 = new NumberCard("yellow", 8);
+	NumberCard y91 = new NumberCard("yellow", 9);
+	NumberCard y92 = new NumberCard("yellow", 9);
+	
+	// blue cards
+	NumberCard b00 = new NumberCard("blue", 0);
+	NumberCard b11 = new NumberCard("blue", 1);
+	NumberCard b12 = new NumberCard("blue", 1);
+	NumberCard b21 = new NumberCard("blue", 2);
+	NumberCard b22 = new NumberCard("blue", 2);
+	NumberCard b31 = new NumberCard("blue", 3);
+	NumberCard b32 = new NumberCard("blue", 3);
+	NumberCard b41 = new NumberCard("blue", 4);
+	NumberCard b42 = new NumberCard("blue", 4);
+	NumberCard b51 = new NumberCard("blue", 5);
+	NumberCard b52 = new NumberCard("blue", 5);
+	NumberCard b61 = new NumberCard("blue", 6);
+	NumberCard b62 = new NumberCard("blue", 6);
+	NumberCard b71 = new NumberCard("blue", 7);
+	NumberCard b72 = new NumberCard("blue", 7);
+	NumberCard b81 = new NumberCard("blue", 8);
+	NumberCard b82 = new NumberCard("blue", 8);
+	NumberCard b91 = new NumberCard("blue", 9);
+	NumberCard b92 = new NumberCard("blue", 9);
+	
+	// red cards
+	NumberCard r00 = new NumberCard("red", 0);
+	NumberCard r11 = new NumberCard("red", 1);
+	NumberCard r12 = new NumberCard("red", 1);
+	NumberCard r21 = new NumberCard("red", 2);
+	NumberCard r22 = new NumberCard("red", 2);
+	NumberCard r31 = new NumberCard("red", 3);
+	NumberCard r32 = new NumberCard("red", 3);
+	NumberCard r41 = new NumberCard("red", 4);
+	NumberCard r42 = new NumberCard("red", 4);
+	NumberCard r51 = new NumberCard("red", 5);
+	NumberCard r52 = new NumberCard("red", 5);
+	NumberCard r61 = new NumberCard("red", 6);
+	NumberCard r62 = new NumberCard("red", 6);
+	NumberCard r71 = new NumberCard("red", 7);
+	NumberCard r72 = new NumberCard("red", 7);
+	NumberCard r81 = new NumberCard("red", 8);
+	NumberCard r82 = new NumberCard("red", 8);
+	NumberCard r91 = new NumberCard("red", 9);
+	NumberCard r92 = new NumberCard("red", 9);
+	
+	// green cards
+	NumberCard g00 = new NumberCard("green", 0);
+	NumberCard g11 = new NumberCard("green", 1);
+	NumberCard g12 = new NumberCard("green", 1);
+	NumberCard g21 = new NumberCard("green", 2);
+	NumberCard g22 = new NumberCard("green", 2);
+	NumberCard g31 = new NumberCard("green", 3);
+	NumberCard g32 = new NumberCard("green", 3);
+	NumberCard g41 = new NumberCard("green", 4);
+	NumberCard g42 = new NumberCard("green", 4);
+	NumberCard g51 = new NumberCard("green", 5);
+	NumberCard g52 = new NumberCard("green", 5);
+	NumberCard g61 = new NumberCard("green", 6);
+	NumberCard g62 = new NumberCard("green", 6);
+	NumberCard g71 = new NumberCard("green", 7);
+	NumberCard g72 = new NumberCard("green", 7);
+	NumberCard g81 = new NumberCard("green", 8);
+	NumberCard g82 = new NumberCard("green", 8);
+	NumberCard g91 = new NumberCard("green", 9);
+	NumberCard g92 = new NumberCard("green", 9);
+	
+	// Action cards
+	ActionCard ySkip1 = new ActionCard("yellow", "skip");
+	ActionCard ySkip2 = new ActionCard("yellow", "skip");
+	
+	ActionCard yDraw1 = new ActionCard("yellow", "draw");
+	ActionCard yDraw2 = new ActionCard("yellow", "draw");
+	
+	ActionCard yReverse1 = new ActionCard("yellow", "reverse");
+	ActionCard yReverse2 = new ActionCard("yellow", "reverse");
+	
+	// blue actions
+	ActionCard bSkip1 = new ActionCard("blue", "skip");
+	ActionCard bSkip2 = new ActionCard("blue", "skip");
+	
+	ActionCard bDraw1 = new ActionCard("blue", "draw");
+	ActionCard bDraw2 = new ActionCard("blue", "draw");
+	
+	ActionCard bReverse1 = new ActionCard("blue", "reverse");
+	ActionCard bReverse2 = new ActionCard("blue", "reverse");
+	
+	// red actions
+	ActionCard rSkip1 = new ActionCard("red", "skip");
+	ActionCard rSkip2 = new ActionCard("red", "skip");
+	
+	ActionCard rDraw1 = new ActionCard("red", "draw");
+	ActionCard rDraw2 = new ActionCard("red", "draw");
+	
+	ActionCard rReverse1 = new ActionCard("red", "reverse");
+	ActionCard rReverse2 = new ActionCard("red", "reverse");
+	
+	// green actions
+	ActionCard gSkip1 = new ActionCard("green", "skip");
+	ActionCard gSkip2 = new ActionCard("green", "skip");
+	
+	ActionCard gDraw1 = new ActionCard("green", "draw");
+	ActionCard gDraw2 = new ActionCard("green", "draw");
+	
+	ActionCard gReverse1 = new ActionCard("green", "reverse");
+	ActionCard gReverse2 = new ActionCard("green", "reverse");
+	
+	// wildcards
+	ActionCard wildDraw1 = new ActionCard("black", "wildDrawFour");
+	ActionCard wildDraw2 = new ActionCard("black", "wildDrawFour");
+	ActionCard wildDraw3 = new ActionCard("black", "wildDrawFour");
+	ActionCard wildDraw4 = new ActionCard("black", "wildDrawFour");
+	
+	ActionCard wild1 = new ActionCard("black", "wild");
+	ActionCard wild2 = new ActionCard("black", "wild");
+	ActionCard wild3 = new ActionCard("black", "wild");
+	ActionCard wild4 = new ActionCard("black", "wild");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
